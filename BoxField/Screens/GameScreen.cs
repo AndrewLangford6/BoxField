6e0b0d020490f7fec81156ac73f7c92fb524b7db
Andrew Langford <Andrew.Langford.6@gmail.com>
2020-02-24 12:43:01 -0500

    bean

--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -28,7 +28,10 @@ namespace BoxField
         //box left list
         List<box> leftBoxes = new List<box>();
 
-         int counter;
+        List<box> rightBoxes = new List<box>();
+
+        int counter;
+        int counter2;
 
         Random ranGen = new Random();
 
@@ -48,7 +51,10 @@ namespace BoxField
             //TODO - set game start values
             box b = new box(red,4, 36, 10);
             leftBoxes.Add(b);
-            
+
+            box b2 = new box(red, 30, 36, 10);
+            rightBoxes.Add(b2);
+
 
 
             Hero = new box(this.Width/2 -8, this.Height - 20, 16);
@@ -91,19 +97,29 @@ namespace BoxField
                 b.Fall();
                 
             }
-            
+            foreach (box b in rightBoxes)
+            {
+                b.Fall();
+
+            }
+
             //TODO - remove box if it has gone of screen
 
-            if (leftBoxes[0].y > this.Height + 200)
+            if (leftBoxes[0].y > this.Height)
             {
                 leftBoxes.RemoveAt(0);
+            }
+            if (rightBoxes[0].y > this.Height)
+            {
+                rightBoxes.RemoveAt(0);
             }
 
 
             //TODO - add new box if it is time
 
-            
+
             counter++;
+            counter2++;
             if(counter == 1)
             {
                 counter = 0;
@@ -148,10 +164,60 @@ namespace BoxField
 
             }
 
-            if(leftArrowDown)
+            //right
+            if (counter2 == 1)
+            {
+                counter2 = 0;
+                int rand2 = ranGen.Next(1, 6);
+
+                if (rand2 == 1)
+                {
+                    int rando = ranGen.Next(-1000, 2000);
+                    box rbox = new box(red, rando, 0, 10);
+                    rightBoxes.Add(rbox);
+
+                }
+                else if (rand2 == 2)
+                {
+                    int rando = ranGen.Next(-1000, 2000);
+                    box rbox = new box(blue, rando, 0, 10);
+                    rightBoxes.Add(rbox);
+
+                }
+                else if (rand2 == 3)
+                {
+                    int rando = ranGen.Next(-1000, 2000);
+                    box rbox = new box(orange, rando, 0, 10);
+                    rightBoxes.Add(rbox);
+
+                }
+                else if (rand2 == 4)
+                {
+                    int rando = ranGen.Next(-1000, 2000);
+                    box rbox = new box(green, rando, 0, 10);
+                    rightBoxes.Add(rbox);
+
+                }
+                else if (rand2 == 5)
+                {
+                    int rando = ranGen.Next(-1000, 2000);
+                    box rbox = new box(purple, rando, 0, 10);
+                    rightBoxes.Add(rbox);
+
+                }
+
+
+            }
+
+            if (leftArrowDown)
             {
                // Hero.Move("left");
                 foreach (box b in leftBoxes)
+                {
+                    b.Move("right");
+
+                }
+                foreach (box b in rightBoxes)
                 {
                     b.Move("right");
 
@@ -162,6 +228,11 @@ namespace BoxField
             {
                 //Hero.Move("right");
                 foreach (box b in leftBoxes)
+                {
+                    b.Move("left");
+
+                }
+                foreach (box b in rightBoxes)
                 {
                     b.Move("left");
 
@@ -178,6 +249,13 @@ namespace BoxField
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             foreach (box b in leftBoxes)
+            {
+
+                e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);
+            }
+
+            //right
+            foreach (box b in rightBoxes)
             {
 
                 e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);

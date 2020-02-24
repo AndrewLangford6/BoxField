5cc9ceca14f0349c79611383ead991f5e0c720be
Andrew Langford <Andrew.Langford.6@gmail.com>
2020-02-24 13:14:14 -0500

    goong

--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -8,34 +8,296 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//namespace BoxField
+//{
+//    public partial class GameScreen : UserControl
+//    {
+//        //player1 button control keys
+//        Boolean leftArrowDown, rightArrowDown;
+
+//        //used to draw boxes on screen
+//        SolidBrush boxBrush = new SolidBrush(Color.PaleGoldenrod);
+
+//        SolidBrush red = new SolidBrush(Color.Red);
+//        SolidBrush blue = new SolidBrush(Color.Blue);
+
+//        SolidBrush green = new SolidBrush(Color.Green);
+//        SolidBrush orange = new SolidBrush(Color.Orange);
+//        SolidBrush purple = new SolidBrush(Color.Purple);
+
+//        //box left list
+//        List<box> leftBoxes = new List<box>();
+
+//        List<box> rightBoxes = new List<box>();
+
+//        int counter;
+//        int counter2;
+
+//        Random ranGen = new Random();
+
+//        box Hero;
+
+//        public GameScreen()
+//        {
+//            InitializeComponent();
+//            OnStart();
+//        }
+
+//        /// <summary>
+//        /// Set initial game values here
+//        /// </summary>
+//        public void OnStart()
+//        {
+//            //TODO - set game start values
+//            box b = new box(red,4, 36, 10);
+//            leftBoxes.Add(b);
+
+//            box b2 = new box(red, 30, 36, 10);
+//            rightBoxes.Add(b2);
+
+
+
+//            Hero = new box(this.Width/2, this.Height/2, 16);
+//        }
+
+//        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+//        {
+//            //player 1 button presses
+//            switch (e.KeyCode)
+//            {
+//                case Keys.Left:
+//                    leftArrowDown = true;
+//                    break;
+//                case Keys.Right:
+//                    rightArrowDown = true;
+//                    break;           
+//            }
+//        }
+
+//        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+//        {
+//            //player 1 button releases
+//            switch (e.KeyCode)
+//            {
+//                case Keys.Left:
+//                    leftArrowDown = false;
+//                    break;
+//                case Keys.Right:
+//                    rightArrowDown = false;
+//                    break;
+//            }
+//        }
+
+//        public void gameLoop_Tick(object sender, EventArgs e)
+//        {
+//            //TODO - update location of all boxes (drop down screen)
+
+//            foreach (box b in leftBoxes)
+//            {
+//                b.Fall();
+
+//            }
+//            foreach (box b in rightBoxes)
+//            {
+//                b.Fall();
+
+//            }
+
+//            //TODO - remove box if it has gone of screen
+
+//            if (leftBoxes[0].y > this.Height)
+//            {
+//                leftBoxes.RemoveAt(0);
+//            }
+//            if (rightBoxes[0].y > this.Height)
+//            {
+//                rightBoxes.RemoveAt(0);
+//            }
+
+
+//            //TODO - add new box if it is time
+
+
+//            counter++;
+//            counter2++;
+//            if(counter == 1)
+//            {
+//                counter = 0;
+//                int rand2 = ranGen.Next(1, 6);
+
+//                if (rand2 == 1)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box box = new box(red, this.Width / 2 - 10, this.Height / 2, 10);
+//                    leftBoxes.Add(box);
+
+//                }
+//                else if (rand2 == 2)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box box = new box(blue, this.Width / 2 - 10, this.Height / 2, 10);
+//                    leftBoxes.Add(box);
+
+//                }
+//                else if (rand2 == 3)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box box = new box(orange , this.Width / 2 - 10, this.Height / 2, 10);
+//                    leftBoxes.Add(box);
+
+//                }
+//                else if (rand2 == 4)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box box = new box(green, this.Width / 2 - 10, this.Height / 2, 10);
+//                    leftBoxes.Add(box);
+
+//                }
+//                else if (rand2 == 5)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box box = new box(purple, this.Width / 2 - 10, this.Height / 2, 10);
+//                    leftBoxes.Add(box);
+
+//                }
+
+
+//            }
+
+//            //right
+//            if (counter2 == 1)
+//            {
+//                counter2 = 0;
+//                int rand2 = ranGen.Next(1, 6);
+
+//                if (rand2 == 1)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box rbox = new box(red, this.Width / 2 + 10, this.Height / 2, 10);
+//                    rightBoxes.Add(rbox);
+
+//                }
+//                else if (rand2 == 2)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box rbox = new box(blue, this.Width / 2 + 10, this.Height / 2, 10);
+//                    rightBoxes.Add(rbox);
+
+//                }
+//                else if (rand2 == 3)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box rbox = new box(orange, this.Width / 2 + 10, this.Height / 2, 10);
+//                    rightBoxes.Add(rbox);
+
+//                }
+//                else if (rand2 == 4)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box rbox = new box(green, this.Width / 2 + 10, this.Height / 2, 10);
+//                    rightBoxes.Add(rbox);
+
+//                }
+//                else if (rand2 == 5)
+//                {
+//                    int rando = ranGen.Next(-1000, 2000);
+//                    box rbox = new box(purple, this.Width/2 + 10, this.Height / 2, 10);
+//                    rightBoxes.Add(rbox);
+
+//                }
+
+
+//            }
+
+//            if (leftArrowDown)
+//            {
+//               // Hero.Move("left");
+//                foreach (box b in leftBoxes)
+//                {
+//                    b.Move("right");
+
+//                }
+//                foreach (box b in rightBoxes)
+//                {
+//                    b.Move("right");
+
+//                }
+
+//            }
+//            if (rightArrowDown)
+//            {
+//                //Hero.Move("right");
+//                foreach (box b in leftBoxes)
+//                {
+//                    b.Move("left");
+
+//                }
+//                foreach (box b in rightBoxes)
+//                {
+//                    b.Move("left");
+
+//                }
+//            }
+//            Refresh();
+//        }
+
+//        private void GameScreen_Load(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void GameScreen_Paint(object sender, PaintEventArgs e)
+//        {
+//            foreach (box b in leftBoxes)
+//            {
+
+//                e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);
+//            }
+
+//            //right
+//            foreach (box b in rightBoxes)
+//            {
+
+//                e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);
+//            }
+
+//            e.Graphics.FillRectangle(boxBrush, Hero.x, Hero.y, Hero.size, Hero.size);
+
+//            //TODO - draw boxes to screen
+//            //
+//        }
+//    }
+//}
 namespace BoxField
 {
     public partial class GameScreen : UserControl
     {
+
+        Random randGen = new Random();
+
         //player1 button control keys
         Boolean leftArrowDown, rightArrowDown;
 
         //used to draw boxes on screen
-        SolidBrush boxBrush = new SolidBrush(Color.PaleGoldenrod);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
 
-        SolidBrush red = new SolidBrush(Color.Red);
-        SolidBrush blue = new SolidBrush(Color.Blue);
+        //create a list to hold a column of boxes        
+        List<box> boxesLeft = new List<box>();
+        List<box> boxesRight = new List<box>();
 
-        SolidBrush green = new SolidBrush(Color.Green);
-        SolidBrush orange = new SolidBrush(Color.Orange);
-        SolidBrush purple = new SolidBrush(Color.Purple);
+        int boxSize = 20;
+        int boxLeftX = 100;
+        int boxGap = 200;
+        int boxXOffset = 5;
 
-        //box left list
-        List<box> leftBoxes = new List<box>();
 
-        List<box> rightBoxes = new List<box>();
+        //counts to see when its time to create a new box
+        int counter = 0;
+        int newBoxCounter = 0;
+        int patternAmount = 10;
 
-        int counter;
-        int counter2;
-
-        Random ranGen = new Random();
-
-        box Hero;
+        //create hero values
+        box hero;
 
         public GameScreen()
         {
@@ -48,16 +310,16 @@ namespace BoxField
         /// </summary>
         public void OnStart()
         {
-            //TODO - set game start values
-            box b = new box(red,4, 36, 10);
-            leftBoxes.Add(b);
+            //set game start values
+            box b1 = new box(boxLeftX, 0, boxSize);
+            boxesLeft.Add(b1);
 
-            box b2 = new box(red, 30, 36, 10);
-            rightBoxes.Add(b2);
+            box b2 = new box(boxLeftX + boxGap, 0, boxSize);
+            boxesRight.Add(b2);
 
+            newBoxCounter++;
 
-
-            Hero = new box(this.Width/2, this.Height/2, 16);
+            hero = new box(50, 300, 20);
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -70,7 +332,7 @@ namespace BoxField
                     break;
                 case Keys.Right:
                     rightArrowDown = true;
-                    break;           
+                    break;
             }
         }
 
@@ -88,183 +350,79 @@ namespace BoxField
             }
         }
 
-        public void gameLoop_Tick(object sender, EventArgs e)
+        private void gameLoop_Tick(object sender, EventArgs e)
         {
-            //TODO - update location of all boxes (drop down screen)
-            
-            foreach (box b in leftBoxes)
+            //update location of all boxes (drop down screen)
+            foreach (box b in boxesLeft)
             {
                 b.Fall();
-                
             }
-            foreach (box b in rightBoxes)
+
+            foreach (box b in boxesRight)
             {
                 b.Fall();
-
             }
 
-            //TODO - remove box if it has gone of screen
-
-            if (leftBoxes[0].y > this.Height)
+            //remove box if it has gone of screen
+            if (boxesLeft[0].y > 400)
             {
-                leftBoxes.RemoveAt(0);
-            }
-            if (rightBoxes[0].y > this.Height)
-            {
-                rightBoxes.RemoveAt(0);
+                boxesLeft.RemoveAt(0);
+                boxesRight.RemoveAt(0);
             }
 
-
-            //TODO - add new box if it is time
-
-
+            //add new box if it is time
             counter++;
-            counter2++;
-            if(counter == 1)
+            if (counter == 9)
             {
+                newBoxCounter++;
+
+                boxLeftX += boxXOffset;
+
+                box b1 = new box(boxLeftX, 0, boxSize);
+                boxesLeft.Add(b1);
+
+                box b2 = new box(boxLeftX + boxGap, 0, boxSize);
+                boxesRight.Add(b2);
+
                 counter = 0;
-                int rand2 = ranGen.Next(1, 6);
 
-                if (rand2 == 1)
+                if (newBoxCounter == patternAmount)
                 {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box box = new box(red, this.Width / 2 - 10, this.Height / 2, 10);
-                    leftBoxes.Add(box);
-                    
+                    boxXOffset = -boxXOffset;
+                    newBoxCounter = 0;
+
+                    patternAmount = randGen.Next(1, 8);
                 }
-                else if (rand2 == 2)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box box = new box(blue, this.Width / 2 - 10, this.Height / 2, 10);
-                    leftBoxes.Add(box);
-
-                }
-                else if (rand2 == 3)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box box = new box(orange , this.Width / 2 - 10, this.Height / 2, 10);
-                    leftBoxes.Add(box);
-
-                }
-                else if (rand2 == 4)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box box = new box(green, this.Width / 2 - 10, this.Height / 2, 10);
-                    leftBoxes.Add(box);
-
-                }
-                else if (rand2 == 5)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box box = new box(purple, this.Width / 2 - 10, this.Height / 2, 10);
-                    leftBoxes.Add(box);
-
-                }
-
-
             }
 
-            //right
-            if (counter2 == 1)
-            {
-                counter2 = 0;
-                int rand2 = ranGen.Next(1, 6);
-
-                if (rand2 == 1)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box rbox = new box(red, this.Width / 2 + 10, this.Height / 2, 10);
-                    rightBoxes.Add(rbox);
-
-                }
-                else if (rand2 == 2)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box rbox = new box(blue, this.Width / 2 + 10, this.Height / 2, 10);
-                    rightBoxes.Add(rbox);
-
-                }
-                else if (rand2 == 3)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box rbox = new box(orange, this.Width / 2 + 10, this.Height / 2, 10);
-                    rightBoxes.Add(rbox);
-
-                }
-                else if (rand2 == 4)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box rbox = new box(green, this.Width / 2 + 10, this.Height / 2, 10);
-                    rightBoxes.Add(rbox);
-
-                }
-                else if (rand2 == 5)
-                {
-                    int rando = ranGen.Next(-1000, 2000);
-                    box rbox = new box(purple, this.Width/2 + 10, this.Height / 2, 10);
-                    rightBoxes.Add(rbox);
-
-                }
-
-
-            }
-
+            //move hero
             if (leftArrowDown)
             {
-               // Hero.Move("left");
-                foreach (box b in leftBoxes)
-                {
-                    b.Move("right");
-
-                }
-                foreach (box b in rightBoxes)
-                {
-                    b.Move("right");
-
-                }
-
+                hero.Move("left");
             }
+
             if (rightArrowDown)
             {
-                //Hero.Move("right");
-                foreach (box b in leftBoxes)
-                {
-                    b.Move("left");
-
-                }
-                foreach (box b in rightBoxes)
-                {
-                    b.Move("left");
-
-                }
+                hero.Move("right");
             }
+
             Refresh();
-        }
-
-        private void GameScreen_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
-            foreach (box b in leftBoxes)
+            //draw boxes to screen
+            foreach (box b in boxesLeft)
             {
-
-                e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);
+                e.Graphics.FillRectangle(b.boxBrush, b.x, b.y, b.size, b.size);
             }
 
-            //right
-            foreach (box b in rightBoxes)
+            foreach (box b in boxesRight)
             {
-
-                e.Graphics.FillRectangle(b.bBrush, b.x, b.y, b.size, b.size);
+                e.Graphics.FillRectangle(b.boxBrush, b.x, b.y, b.size, b.size);
             }
 
-            e.Graphics.FillRectangle(boxBrush, Hero.x, Hero.y, Hero.size, Hero.size);
-
-            //TODO - draw boxes to screen
-            //
+            e.Graphics.FillRectangle(whiteBrush, hero.x, hero.y, hero.size, hero.size);
         }
     }
 }
--- BoxField/box.cs
+++ BoxField/box.cs
@@ -6,44 +6,99 @@ using System.Threading.Tasks;
 using System.Drawing;
 
 
+//namespace BoxField
+//{
+//    class box
+//    {
+//        public SolidBrush bBrush;
+
+
+//        public int x, y;
+//       public int size;
+
+//        public box(int _x, int _y, int _size)
+//        {
+//            x = _x;
+//            y = _y;
+//            size = _size;
+//        }
+//        public box(SolidBrush _brushbox, int _x, int _y, int _size)
+//        {
+//            x = _x;
+//            y = _y;
+//            size = _size;
+//            bBrush = _brushbox;
+//        }
+
+//        public void Fall()
+//        {
+//            y = y + 3;
+//        }
+//        public void Move(string direction)
+//        {
+//            if (direction == "left")
+//            {
+//                x = x - 5;
+//            }
+//            if (direction == "right")
+//            {
+//                x = x + 5;
+//            }
+//        }
+
+//    }
+//}
 namespace BoxField
 {
     class box
     {
-        public SolidBrush bBrush;
-        
-
-        public int x, y;
-       public int size;
+        public SolidBrush boxBrush;
+        public int x, y, size;
+        Random randGen = new Random();
 
         public box(int _x, int _y, int _size)
         {
             x = _x;
             y = _y;
             size = _size;
+
+            int randValue = randGen.Next(1, 3);
+
+            if (randValue == 1)
+            {
+                boxBrush = new SolidBrush(Color.Red);
+            }
+            else if (randValue == 2)
+            {
+                boxBrush = new SolidBrush(Color.Orange);
+            }
         }
-        public box(SolidBrush _brushbox, int _x, int _y, int _size)
+
+        public box(SolidBrush _boxBrush, int _x, int _y, int _size)
         {
             x = _x;
             y = _y;
             size = _size;
-            bBrush = _brushbox;
+            boxBrush = _boxBrush;
         }
 
         public void Fall()
         {
             y = y + 3;
         }
+
         public void Move(string direction)
         {
             if (direction == "left")
             {
                 x = x - 5;
             }
+
             if (direction == "right")
             {
                 x = x + 5;
             }
+
         }
 
     }
